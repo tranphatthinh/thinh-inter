af9ec2af6aab3f121333be03f470a6028b1f194b
thinh <tranphatthinh15052004@gmail.com>
2026-04-21 10:37:19 +0700

    add app/Http/Controllers/PostController file

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    //
+    public function index()
+    {
+        return Post::all();
+    }
+    
+    public function store(Request $request)
+    {
+        return Post::create($request->all());
+    }
+    
+    public function show($id)
+    {
+        return Post::findOrFail($id);
+    }
+    
+    public function update(Request $request, $id)
+    {
+        $post = Post::findOrFail($id);
+        $post-> update($request->all());
+        return $post;
+    }
+
+    public function delete($id)
+    {
+        Post::destroy($id);
+        return response()->json (['messeage'=>'Deleted']);
+    }
+}
