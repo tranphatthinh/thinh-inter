9a105b98a9fe95999d6206708d243eb3853bab9b
thinh <tranphatthinh15052004@gmail.com>
2026-04-21 09:29:28 +0700

    add app/Http/Controller/ProductController file

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     //
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        Product::created($request->all());
+    }
+
+    public function show($id)
+    {
+        return Product::find0rFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $student = Product::findOrFail($id);
+        $student->update($request->all());
+        return $student;
+    }
+
+    public function destroy($id)
+    {
+        Product::destroy($id);
+        return response()->json(['message' => 'Deleted']);
+    }
+
 }
