6c141797fc71064b55b01ad8a23108f96bd7cee7
thinh <tranphatthinh15052004@gmail.com>
2026-04-21 08:32:22 +0700

    upgrade routes/api.php file

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::apiResource('students', StudentController::class);
+Route::get('/students', [StudentController::class, 'index']);
+Route::get('/students/{id}', [StudentController::class, 'show']);
+Route::post('/students', [StudentController::class, 'store']);
+Route::put('/students/{id}', [StudentController::class, 'update']);
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
