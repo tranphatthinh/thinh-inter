f36f9beb22c42c3eaa6ab976f5899e4d489b9d41
thinh <tranphatthinh15052004@gmail.com>
2026-04-21 09:34:51 +0700

    update routes/api.php  file

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,4 +8,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::apiResource('students', StudentController::class);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
