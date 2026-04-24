18223300d08081e72b8f5332d3f1080f43006b7b
thinh <tranphatthinh15052004@gmail.com>
2026-04-24 13:23:23 +0700

    update routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\KhoaController;
 use Illuminate\Support\Facades\Route;
+use League\Config\ReadOnlyConfiguration;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/khoas', [KhoaController::class, 'index']);
+Route::get('/khoas/{id}', [KhoaController::class, 'show']);
+Route::post('/khoas', [KhoaController::class, 'store']);
+Route::put('/khoas/{id}', [KhoaController::class, 'update']);
+Route::delete('/khoas/{id}', [KhoaController::class, 'destroy']);
