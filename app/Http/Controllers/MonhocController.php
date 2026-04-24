6124bb7dbb117f53d1bf49629bca27a54ff0aa1a
thinh <tranphatthinh15052004@gmail.com>
2026-04-24 14:13:43 +0700

    add app/Http/Controllers/MonhocController.php

---
+++ app/Http/Controllers/MonhocController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Monhoc;
+
+class MonhocController extends Controller
+{
+    //
+    public function index()
+    {
+        return Monhoc::all();
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'mamon'=> 'required|string|max:50|unique:monhocs',
+            'tenmon'=> 'required|string|max:255',
+            'sotinchi'=> 'required|integer|min:1|max:10'
+        ]);
+
+        $monhoc = Monhoc::create($data);
+        return response()->json([
+            'message'=> 'da them thanh cong',
+            'data' => $monhoc
+        ],201);
+    }
+    
+    public function show($id)
+    {
+        return Monhoc::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $monhoc = Monhoc::findOrFail($id);
+        $data = $request->validate([
+            'mamon' => 'sometimes|string|max:50|unique:monhocs, mamon'. $id,
+            'tenmon'=> 'sometimes|string|max:255',
+            'sotinchi' => 'sometimes|integer|min:1|max:10'
+        ]);
+        $monhoc->update($data);
+        return response()->json([
+            'message' => 'cap nhat thanh cong',
+            'data' => $monhoc
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        Monhoc::findOrFail($id)->delete();
+        return response()->json([
+            'message'=> 'da xoa thanh cong'
+        ]);
+    }
+}
