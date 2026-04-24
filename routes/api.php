96dee0b3a755a040b552b09adef672e996657902
thinh <tranphatthinh15052004@gmail.com>
2026-04-24 14:19:13 +0700

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\MonhocController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/monhocs', [MonhocController::class, 'index']);
+Route::get('/monhocs/{id}', [MonhocController::class, 'show']);
+Route::post('/monhocs', [MonhocController::class, 'store']);
+Route::put('/monhocs/{id}', [MonhocController::class, 'update']);
+Route::delete('/monhocs/{id}', [MonhocController::class, 'destroy']);
