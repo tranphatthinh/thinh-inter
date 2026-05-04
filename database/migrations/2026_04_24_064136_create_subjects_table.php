81dd0bbfc568013c5bc9a70ad8817b89fd7eab4e
thinh <tranphatthinh15052004@gmail.com>
2026-05-04 18:57:44 +0700

    fix bug

--- database/migrations/2026_04_24_064136_create_subjects_table.php
+++ database/migrations/2026_04_24_064136_create_subjects_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('monhocs', function (Blueprint $table) {
+        Schema::create('supjects', function (Blueprint $table) {
             $table->id();
             $table-> string('mamon')->unique();
             $table-> string('tenmon');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('monhocs');
+        Schema::dropIfExists('supjects');
     }
 };
